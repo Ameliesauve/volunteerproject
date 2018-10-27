234e82f6593f0e27ed933b8cdb3fe147bc0a3bbe
ameliesauve <dagostino.amelie@gmail.com>
2018-10-27 10:34:39 -0300

    backend structure started

---
+++ app.js
@@ -0,0 +1,23 @@
+const routes = require('./routes');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var nunjucks = require('nunjucks');
+var path = require('path');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'html'); // hace que res.render funcione con archivos html
+app.engine('html', nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
+nunjucks.configure('views', { noCache: true }); // apunta a nunjucks al directorio correcto para los templates
+
+app.use('/', routes);
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/', function(req, res, next) {
+  //   console.log('hicieron un request de ' + req.url);
+  next();
+});
+
+app.listen(3000);
